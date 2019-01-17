609fcf2286d0e605b26c072061ec625a267ea502
Michael Haynes <mf.haynes@gmail.com>
2019-01-17 22:09:41 +0000

    drop new objects in cleanup

--- step_000_cleanup.sql
+++ step_000_cleanup.sql
@@ -4,3 +4,5 @@ drop user canary_sch cascade;
 drop user canary_app cascade;
 drop tablespace canary_ts;
 drop edition release1 cascade;
+drop trigger edition_router_trig;
+drop function get_random_edition;

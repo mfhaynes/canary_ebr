ba96be4061a3d9d0f188adc4bca6e956832452a1
Michael Haynes <mf.haynes@gmail.com>
2019-01-17 22:27:25 +0000

    update demo code

--- apps/demo1_query_things.py
+++ apps/demo1_query_things.py
@@ -12,6 +12,7 @@ def make_connection(p_password):
     v_start_time = datetime.now()
     return v_connection, v_cursor, v_edition, v_start_time
 
+v_edition_results = {}
 v_password = getpass.getpass('Enter password: ')
 v_connection, v_cursor, v_edition, v_start_time = make_connection(v_password)
 v_cursor.execute ('select object_id from all_objects');
@@ -26,8 +27,26 @@ for v_object_key in v_object_keys:
     v_thing = v_cursor.fetchone()
     if v_counter == 1000:
         v_total_time = datetime.now()-v_start_time
+        if v_edition in v_edition_results.keys():
+            v_edition_results[v_edition]['executions'] += 1
+            v_edition_results[v_edition]['run_time'] += v_total_time
+        else:
+            v_edition_results[v_edition] = {'executions': 1,
+                                            'run_time': v_total_time}
         print(v_edition.ljust(30) + str(v_total_time))
         v_counter = 0
         # Close and re-open the connection for demo purposes.
         v_connection.close()
         v_connection, v_cursor, v_edition, v_start_time = make_connection(v_password)
+
+print ('Edition'.ljust(30)
+       + ' Runs   '
+       + 'Total Run Time   '
+       + '  Avg Run Time')
+for v_edition in v_edition_results.keys():
+    print (v_edition.ljust(30) 
+           + str(v_edition_results[v_edition]['executions']).rjust(5)
+           + '   '
+           + str(v_edition_results[v_edition]['run_time'])
+           + '   '
+           + str(v_edition_results[v_edition]['run_time']/v_edition_results[v_edition]['executions']))

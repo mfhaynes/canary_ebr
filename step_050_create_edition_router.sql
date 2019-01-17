85d2fc80ff47fa627e34f7cf40bcf95dec622e4d
Michael Haynes <mf.haynes@gmail.com>
2019-01-17 22:11:44 +0000

    fix typo

--- step_050_create_edition_router.sql
+++ step_050_create_edition_router.sql
@@ -2,7 +2,7 @@ CREATE TABLE edition_router
 (edition_name varchar2(128) NOT NULL,
  percentage number(3,2) NOT NULL,
  CONSTRAINT check_percentage
- CHECK (percentage >=0 and percentage <= 1)
+ CHECK (percentage >=0 and percentage <= 1))
 TABLESPACE canary_ts;
 CREATE UNIQUE INDEX edition_router_pkidx
 ON edition_router (edition_name)

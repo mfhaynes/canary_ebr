00a008ca893ba562af940fd9219c78f7490bc203
Michael Haynes <mf.haynes@gmail.com>
2019-01-23 02:07:20 +0000

    tidy up spacing

--- apps/demo2_query_things.py
+++ apps/demo2_query_things.py
@@ -26,40 +26,40 @@ while [ 1 == 1 ]:
     for v_object_key in v_object_keys:
         v_counter += 1
         v_app_query = """select t.thing_id, t.thing_name, t.thing_create_date,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'object_type') AS object_type,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'editionable_flag') AS editionable_flag,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'status') AS status,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'temporary_flag') AS temporary,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'generated_flag') AS generated,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'secondary_flag') AS secondary,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'timestamp') AS timestamp,
-                          (SELECT ta.thing_attribute_value
-                           FROM canary_sch.ebr_thing_attributes ta
-                           WHERE ta.thing_id = t.thing_id
-                             AND ta.thing_attribute_type = 'default_collation') AS default_collation
-                          from canary_sch.ebr_things t
-                          where t.thing_id = :thing
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'object_type') AS object_type,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'editionable_flag') AS editionable_flag,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'status') AS status,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'temporary_flag') AS temporary,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'generated_flag') AS generated,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'secondary_flag') AS secondary,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'timestamp') AS timestamp,
+                         (SELECT ta.thing_attribute_value
+                          FROM canary_sch.ebr_thing_attributes ta
+                          WHERE ta.thing_id = t.thing_id
+                            AND ta.thing_attribute_type = 'default_collation') AS default_collation
+                         from canary_sch.ebr_things t
+                         where t.thing_id = :thing
         """
         v_cursor.execute(v_app_query, {'thing': v_object_key[0]})
         v_thing = v_cursor.fetchone()

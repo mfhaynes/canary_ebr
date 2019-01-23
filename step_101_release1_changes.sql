c4b1e990529fdd33d47ca3cdd1aceb1443791ea0
Michael Haynes <mf.haynes@gmail.com>
2019-01-23 22:56:22 +0000

    reduce transaction size in update

--- step_101_release1_changes.sql
+++ step_101_release1_changes.sql
@@ -8,40 +8,58 @@ ADD (object_type varchar2(30),
      secondary_flag varchar2(1),
      timestamp varchar2(19),
      default_collation varchar2(100));
-UPDATE ebr_things_b thing
-SET object_type = (SELECT thing_attribute_value
-                   FROM ebr_thing_attributes_b attr
-                   WHERE thing.thing_id = attr.thing_id
-                     AND thing_attribute_type = 'object_type'),
-    editionable_flag = (SELECT thing_attribute_value
-                        FROM ebr_thing_attributes_b attr
-                        WHERE thing.thing_id = attr.thing_id
-                          AND thing_attribute_type = 'editionable_flag'),
-    status = (SELECT thing_attribute_value
-              FROM ebr_thing_attributes_b attr
-              WHERE thing.thing_id = attr.thing_id
-                AND thing_attribute_type = 'status'),
-    temporary_flag = (SELECT thing_attribute_value
+DECLARE
+    v_continue boolean;
+BEGIN
+    v_continue := TRUE;
+    WHILE v_continue
+    LOOP
+        UPDATE ebr_things_b thing
+        SET object_type = (SELECT thing_attribute_value
+                           FROM ebr_thing_attributes_b attr
+                           WHERE thing.thing_id = attr.thing_id
+                             AND thing_attribute_type = 'object_type'),
+            editionable_flag = (SELECT thing_attribute_value
+                                FROM ebr_thing_attributes_b attr
+                                WHERE thing.thing_id = attr.thing_id
+                                  AND thing_attribute_type = 'editionable_flag'),
+            status = (SELECT thing_attribute_value
                       FROM ebr_thing_attributes_b attr
                       WHERE thing.thing_id = attr.thing_id
-                        AND thing_attribute_type = 'temporary_flag'),
-    generated_flag = (SELECT thing_attribute_value
-                      FROM ebr_thing_attributes_b attr
-                      WHERE thing.thing_id = attr.thing_id
-                        AND thing_attribute_type = 'generated_flag'),
-    secondary_flag = (SELECT thing_attribute_value
-                      FROM ebr_thing_attributes_b attr
-                      WHERE thing.thing_id = attr.thing_id
-                        AND thing_attribute_type = 'secondary_flag'),
-    timestamp = (SELECT thing_attribute_value
-                 FROM ebr_thing_attributes_b attr
-                 WHERE thing.thing_id = attr.thing_id
-                   AND thing_attribute_type = 'timestamp'),
-    default_collation = (SELECT thing_attribute_value
+                        AND thing_attribute_type = 'status'),
+            temporary_flag = (SELECT thing_attribute_value
+                              FROM ebr_thing_attributes_b attr
+                              WHERE thing.thing_id = attr.thing_id
+                                AND thing_attribute_type = 'temporary_flag'),
+            generated_flag = (SELECT thing_attribute_value
+                              FROM ebr_thing_attributes_b attr
+                              WHERE thing.thing_id = attr.thing_id
+                                AND thing_attribute_type = 'generated_flag'),
+            secondary_flag = (SELECT thing_attribute_value
+                              FROM ebr_thing_attributes_b attr
+                              WHERE thing.thing_id = attr.thing_id
+                                AND thing_attribute_type = 'secondary_flag'),
+            timestamp = (SELECT thing_attribute_value
                          FROM ebr_thing_attributes_b attr
                          WHERE thing.thing_id = attr.thing_id
-                           AND thing_attribute_type = 'default_collation');
-COMMIT;
+                           AND thing_attribute_type = 'timestamp'),
+            default_collation = (SELECT thing_attribute_value
+                                 FROM ebr_thing_attributes_b attr
+                                 WHERE thing.thing_id = attr.thing_id
+                                   AND thing_attribute_type = 'default_collation')
+        WHERE rownum <= 1000
+          AND object_type is NULL;
+    
+        IF SQL%ROWCOUNT > 0 THEN
+            v_continue := TRUE;
+        ELSE
+            v_continue := FALSE;
+        END IF;
+
+        COMMIT;
+    END LOOP;
+END;
+/
 CREATE OR REPLACE EDITIONING VIEW ebr_things AS 
 SELECT thing_id, thing_name, thing_create_date,
        object_type, editionable_flag, status,

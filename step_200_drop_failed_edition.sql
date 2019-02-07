a21c883f775d1a920b133f118ecb4aa6bc332796
Michael Haynes <mf.haynes@gmail.com>
2019-02-07 21:39:44 +0000

    cleanup scripts

---
+++ step_200_drop_failed_edition.sql
@@ -0,0 +1,4 @@
+prompt Connect as system ID
+@../private/connect_system.sql
+set echo on
+drop edition release1 cascade;
---
+++ step_999_post_demo_1_cleanup.sql
@@ -0,0 +1,6 @@
+prompt Connect as system ID
+@../private/connect_system.sql
+set echo on
+drop user canary_sch cascade;
+drop user canary_app cascade;
+drop tablespace canary_ts;

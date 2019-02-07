3994bf25e56687610645135b66dc06059fca7755
Michael Haynes <mf.haynes@gmail.com>
2019-02-07 21:32:56 +0000

    script updates

--- step_011_show_data_details.sql
+++ step_011_show_data_details.sql
@@ -1,4 +1,9 @@
 set echo on
+set lines 140
+set pages 30
+col thing_name for a30
+set pause on
+
 select count(*) from canary_sch.ebr_things;
 
 select count(*) from canary_sch.ebr_thing_attributes;
@@ -11,3 +16,5 @@ from canary_sch.ebr_thing_attributes attr
 join canary_sch.ebr_things t on t.thing_id = attr.thing_id
 where t.thing_name = 'GUMBALL'
 order by attr.thing_attribute_type;
+
+set pause off

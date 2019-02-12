9a1411a09170118327b7c6ab08a49282fe575f3b
Michael Haynes <mf.haynes@gmail.com>
2019-02-12 19:01:00 +0000

    update finalize script

--- step_301_demo2_finalize_successful_edition.sql
+++ step_301_demo2_finalize_successful_edition.sql
@@ -1,11 +1,21 @@
 @../private/connect_system.sql
+col edition_name for a40
 set echo on
-pause show DEFAULT_EDITION
-select property_value
-from database_properties
-where property_name = 'DEFAULT_EDITION';
+alter session set edition=release1;
 
 pause show DEFAULT_EDITION
 select property_value
 from database_properties
 where property_name = 'DEFAULT_EDITION';
+
+pause set DEFAULT_EDITION
+ALTER DATABASE DEFAULT EDITION=release1;
+
+pause show DEFAULT_EDITION
+select property_value
+from database_properties
+where property_name = 'DEFAULT_EDITION';
+
+pause drop old edition
+PURGE DBA_RECYCLEBIN;
+DROP EDITION ORA$BASE CASCADE;

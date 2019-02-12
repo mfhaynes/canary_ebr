7deafb8e765edbb0f8df8a9ef101c7b2333c7595
Michael Haynes <mf.haynes@gmail.com>
2019-02-12 18:41:59 +0000

    add pause comment

--- step_105_demo2_update_edition_router.sql
+++ step_105_demo2_update_edition_router.sql
@@ -3,4 +3,6 @@ SET percentage = .75
 WHERE edition_name = 'ORA$BASE';
 INSERT INTO canary_sch.edition_router
 VALUES ('release1', .25);
+
+PAUSE Once we commit, the change will take effect.
 COMMIT;

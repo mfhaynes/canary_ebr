16b154266a93cc093d7ef6007d736f611c32c9f1
Michael Haynes <mf.haynes@gmail.com>
2019-02-13 14:25:15 +0000

    add query

--- step_102_demo2_release1_app_view_change.sql
+++ step_102_demo2_release1_app_view_change.sql
@@ -3,3 +3,6 @@ SELECT thing_id, thing_name, thing_create_date,
        attribute1, attribute2, attribute3, attribute4,
        flag1, flag2, flag3, flag4
 FROM ebr_things;
+
+pause show current object editions
+select object_name, edition_name from dba_objects where edition_name is not null;

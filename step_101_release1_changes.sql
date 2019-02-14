48d1e20f168de4db445555076eb4f1dd5d00e218
Michael Haynes <mf.haynes@gmail.com>
2019-02-14 18:56:28 +0000

    add object_type to query

--- step_101_release1_changes.sql
+++ step_101_release1_changes.sql
@@ -2,10 +2,11 @@ pause Connect with explicit EDITION setting
 connect canary_sch@&database edition=release1
 
 col object_name for a30
+col object_type for a20
 col edition_name for a30
 
 pause show current object editions
-select object_name, edition_name from dba_objects where edition_name is not null;
+select object_name, edition_name, edition_type from dba_objects where edition_name is not null;
 
 pause Add New Columns to THINGS table.
 ALTER TABLE ebr_things_b
@@ -84,4 +85,4 @@ pause DROP editioning view not needed in new edition
 DROP VIEW ebr_thing_attributes;
 
 pause show current object editions
-select object_name, edition_name from dba_objects where edition_name is not null;
+select object_name, edition_name, edition_type from dba_objects where edition_name is not null;
--- step_102_demo2_release1_app_view_change.sql
+++ step_102_demo2_release1_app_view_change.sql
@@ -5,4 +5,4 @@ SELECT thing_id, thing_name, thing_create_date,
 FROM ebr_things;
 
 pause show current object editions
-select object_name, edition_name from dba_objects where edition_name is not null;
+select object_name, edition_name, edition_type from dba_objects where edition_name is not null;
